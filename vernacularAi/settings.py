e189bba4778d74bab6b5859bbccfadf6456e6141
Ashutosh Jha <aj97389@gmail.com>
2021-07-02 13:36:22 +0530

    Adds docker related files and requirements.txt

--- vernacularAi/settings.py
+++ vernacularAi/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'assignment'
+    'assignment',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
