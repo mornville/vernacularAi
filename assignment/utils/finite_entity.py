e404814b04888499a7b1ecd289d0533b2bd80847
Ashutosh Jha <aj97389@gmail.com>
2021-07-02 12:46:39 +0530

    Adds both APIs required

---
+++ assignment/utils/finite_entity.py
@@ -0,0 +1,52 @@
+from . parent import *
+from typing import List
+
+class Finite_Entity(Validate):
+    """
+    Subclass to validate if the values belong to a finite set and extract it
+    """
+    def validate_finite_values_entity(self, values: List[Dict], supported_values: List[str] = None,
+                                invalid_trigger: str = None, key: str = None,
+                                support_multiple: bool = True, pick_first: bool = False, **kwargs) -> SlotValidationResult:
+        """
+        Validate an entity on the basis of its value extracted.
+        The method will check if the values extracted("values" arg) lies within the finite list of supported values(arg "supported_values").
+    
+        :param pick_first: Set to true if the first value is to be picked up
+        :param support_multiple: Set to true if multiple utterances of an entity are supported
+        :param values: Values extracted by NLU
+        :param supported_values: List of supported values for the slot
+        :param invalid_trigger: Trigger to use if the extracted value is not supported
+        :param key: Dict key to use in the params returned
+        :return: a tuple of (filled, partially_filled, trigger, params)
+        """
+        
+        valid_values = [] 
+       
+        if not supported_values: 
+            supported_values = []
+
+        if not values:
+            values = []
+
+
+        if len(values) == 0:
+            self.set_trigger(invalid_trigger)
+            self.set_filled(False)
+            self.set_part_filled(False)
+        else:
+            supported_values_set = set(supported_values) # Set provides faster data acces.
+            for each_value in values:
+                if each_value["value"] in supported_values_set:
+                    valid_values.append(each_value["value"].upper())
+                else:
+                    self.set_trigger(invalid_trigger)
+                    self.set_filled(False)
+                    self.set_part_filled(True)
+                    valid_values = []
+                    break
+            self.set_parameters(valid_values, key, support_multiple, pick_first)
+        
+        return self.get_response() 
+
+      
--- assignment/views.py
+++ assignment/views.py
@@ -3,38 +3,59 @@ from django.http.response import HttpResponse
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
-
-ATTRIBUTES = ['values', 'supported_values', 'invalid_trigger', 'key', 'support_multiple', 'pick_first']
-RESPONSE = ['filled', 'partially_filled', 'trigger', 'parameters']
-
+from .utils.numeric_entity import *;
+from .utils.finite_entity import *;
 @api_view(['POST'])
 def finite_entity(request):
-    body = json.loads(request.body.decode('utf-8'))
-    data = {}
-    try:
-        for attr in ATTRIBUTES:
-            data[attr] = body.get(attr)
-            if data[attr] == None:
-                return Response(attr.upper() + " Not sent in body", status=status.HTTP_400_BAD_REQUEST)
+    try: 
+        body = json.loads(request.body.decode('utf-8'))
+
+        data             = json.loads(request.body.decode("utf-8"))
+        values           = body.get("values")
+        supported_values = body.get("supported_values")
+        invalid_trigger  = body.get("invalid_trigger")
+        key              = body.get("key")
+        support_multiple = body.get("support_multiple")
+        pick_first       = body.get("pick_first")
+        validator        = Finite_Entity()
+        filled, partially_filled, trigger, parameters = validator.validate_finite_values_entity(values, supported_values, invalid_trigger, key, support_multiple, pick_first)
+        response = {
+                    "filled" : filled,
+                    "partially_filled" : partially_filled,
+                    "trigger" : trigger,
+                    "parameters" : parameters
+                }
+
+        return Response(response, status=status.HTTP_200_OK)
     except Exception as e:
         return Response("Internal Server Error -> " + str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
-    return Response(json.dumps(data), status=status.HTTP_200_OK)
   
 @api_view(['POST'])
 def numeric_entity(request):
-    body = json.loads(request.body.decode('utf-8'))
-    data = {}
     try:
-        for attr in ATTRIBUTES:
-            data[attr] = body.get(attr)
-            if data[attr] == None:
-                return Response(attr.upper() + " Not sent in body", status=status.HTTP_400_BAD_REQUEST)
+        body             = json.loads(request.body.decode("utf-8"))
+        values           = body.get("values")
+        invalid_trigger  = body.get("invalid_trigger")
+        key              = body.get("key")
+        support_multiple = body.get("support_multiple")
+        pick_first       = body.get("pick_first")
+        constraint       = body.get("constraint")
+        var_name         = body.get("var_name")
+        validator = Numeric_Entity()
+        filled, partially_filled, trigger, parameters = validator.validate_numeric_entity(values, invalid_trigger, key, support_multiple, pick_first, constraint, var_name)
+        response = {
+                    "filled" : filled,
+                    "partially_filled" : partially_filled,
+                    "trigger" : trigger,
+                    "parameters" : parameters
+                }
+
+        return Response(response, status=status.HTTP_200_OK)
     except Exception as e:
         return Response("Internal Server Error -> " + str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
-    return Response(json.dumps(data), status=status.HTTP_200_OK)
 
 @api_view(['GET', 'POST'])
-def index(request):
+def index(request=None):
     return HttpResponse("Welcome, this is working.")
